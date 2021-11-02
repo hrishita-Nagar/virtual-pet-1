7d5ee6d7db95eeea7420f895194de0dbf8176f6c
hrishita-Nagar <87662168+hrishita-Nagar@users.noreply.github.com>
2021-11-02 20:33:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+//Create variables here
+var dog , food ; 
+var foodStock , foods;
+
+function preload()
+{
+	//load images here
+  dogImage = loadImage("images/dogImg.png");
+  happyDogImage = loadImage("images/dogImg1.png");
+  bgImg = loadImage("images/BgImage.jpg")
+
+}
+
+function setup() {
+	createCanvas(700, 450);
+  
+  dog = createSprite(600,350);
+  dog.addImage(dogImage);
+  dog.scale = 0.2;
+
+  database = firebase.database();
+  console.log(database);
+  foodStockRef = database.ref('food');
+  foodStockRef.on("value",readStock);
+  
+}
+
+
+function draw() { 
+  background(bgImg);
+  if(keyWentDown(UP_ARROW)){
+    if(foodStock > 0){
+    foodStock = foodStock - 1;
+    writeStock(foodStock);
+    dog.addImage(happyDogImage);
+    }
+
+  }
+
+  if(foodStock === 0){
+    dog.addImage(dogImage);
+  }
+      
+  drawSprites();
+  textSize(18);
+  fill("black");
+  text("Press and hold UP ARROW key to feed the dog", 50 , 20);
+  text("Food Remaining : " + foodStock, 50, 40);
+}
+
+function readStock(data){
+  foodStock = data.val();
+
+}
+  
+function writeStock(x){
+
+  if(x <= 0){
+    x = 0;
+  }else{
+    x = x - 1;
+  }
+
+  database.ref('/').update({
+    food : x
+  })
+}
